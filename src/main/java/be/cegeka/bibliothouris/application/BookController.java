7f16559faee3efc174a4d7940608b635e46c1dbd
Jeroen Stroobants <jeroen.stroobants@cegeka.com>
2017-11-03 12:00:49 +0100

    bookcontroller test getBookDetails

--- src/main/java/be/cegeka/bibliothouris/application/BookController.java
+++ src/main/java/be/cegeka/bibliothouris/application/BookController.java
@@ -1,12 +1,25 @@
 package be.cegeka.bibliothouris.application;
 
+import be.cegeka.bibliothouris.domain.users.Book;
+import be.cegeka.bibliothouris.domain.users.BookService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.inject.Inject;
 import javax.transaction.Transactional;
 
 @RestController
 @RequestMapping(path = "/book")
 @Transactional
 public class BookController {
+
+    @Inject
+    private BookService bookService;
+
+    @GetMapping(path = "/{id}")
+    public Book getBookDetails(@PathVariable(value = "id") int id) {
+        return bookService.getBookDetails(id);
+    }
 }
---
+++ src/test/java/be/cegeka/bibliothouris/application/BookControllerTest.java
@@ -0,0 +1,30 @@
+package be.cegeka.bibliothouris.application;
+
+import be.cegeka.bibliothouris.domain.users.Book;
+import be.cegeka.bibliothouris.domain.users.BookService;
+import org.junit.Rule;
+import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnit;
+import org.mockito.junit.MockitoRule;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.when;
+
+public class BookControllerTest {
+    @Rule
+    public MockitoRule rule = MockitoJUnit.rule();
+
+    @InjectMocks
+    private BookController bookController;
+
+    @Mock
+    private BookService bookService;
+
+    @Test
+    public void whenGettingBookDetails_ShouldReturnDetailsOfASingleBook() throws Exception {
+    
+    }
+}
